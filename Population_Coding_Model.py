a8d45ea22fdf804ffac3db52200bd052db137ca7
Richard <rjleadbeater@gmail.com>
2021-03-26 12:16:16 +0000

    Initial commit

---
+++ Population_Coding_Model.py
@@ -0,0 +1,25 @@
+
+# define params for tiling of feature space
+
+# define params for stimuli values
+
+# define params for tuning of neural populations
+
+# define params for response of neural populations
+
+# each neural population as instance of class
+    # boundaries
+    # bandwidth
+    # sampling frequency
+    # max firing rate
+    # preferences
+    # genTunings()
+
+# adjust boundaries of each population based on sampling frequencies
+
+# piece together tuningArrays and prefVectors from each population into allPops
+
+# generate response for allPops from stim_vals
+
+# decode allPops responses (WTA, PV, ML)
+
