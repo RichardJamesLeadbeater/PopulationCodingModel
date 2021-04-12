9d52e478cf03ff925397b25619d2b67f97eae6f8
Richard <rjleadbeater@gmail.com>
2021-04-12 17:03:16 +0100

    beginning project

--- Population_Coding_Model.py
+++ Population_Coding_Model.py
@@ -1,19 +1,53 @@
 import numpy as np
 
 
-class Tile():
-    """tiling of feature space"""
+def find_nearest(array, value):  # return value and index of nearest value
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx], idx
 
-    def __init__(self, min, max, stepsize):
-        self.min = min
-        self.max = max
-        self.range = abs(min - max)
+
+def get_indices(array, values):  # nearest index of array for each value
+    indices = [None] * len(values)
+    for idx, ii in enumerate(values):
+        _, indices[idx] = find_nearest(array, ii)
+    return indices
+
+
+class Tiling:
+
+    """tiling of feature space"""
+    def __init__(self, min_val, max_val, stepsize):
+        self.min_val = min_val
+        self.max_val = max_val
+        self.range = abs(min_val - max_val)
         self.stepsize = stepsize
-        self.tiling = np.arange(min, max, stepsize)
+        self.tiling = np.arange(min_val, max_val, stepsize)
+
+
+class Stimuli:
+
+    """values of stimuli across feature space"""
+    def __init__(self, n_stim, min_val, max_val, tiling, distribution):
+        self.n_stim = n_stim
+        self.min_val = min_val
+        self.max_val = max_val
+
+        self.distribution = distribution
+        if distribution == 'unif':
+            self.stim_vals = np.linspace(min_val, max_val, n_stim)
+        elif distribution == 'rand':
+            self.stim_vals = np.random.uniform(min_val, max_val, n_stim)
+
+        self.stim_idx =
+        # self.stim_vals = np.random.choice(self.stim_tiling, n_stim)
+        # _, self.min_idx = find_nearest(tiling, min_val)
+        # _, self.max_idx = find_nearest(tiling, max_val)
+        # self.stim_tiling = tiling[self.min_idx: self.max_idx + 1]  # index into feature space for stimuli range
 
 
 # define params for tiling of feature space
-feature_space = Tile(min=-90, max=90, stepsize=0.05)
+feature_space = Tiling(min_val=-90, max_val=90, stepsize=0.05)
 
 # define params for stimuli values
 
