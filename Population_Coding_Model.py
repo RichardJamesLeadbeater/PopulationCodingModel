76adf92cde120b9faa3eb2dae46b50bebf219257
Richard <rjleadbeater@gmail.com>
2021-04-12 14:02:44 +0100

    beginning project

--- Population_Coding_Model.py
+++ Population_Coding_Model.py
@@ -1,5 +1,19 @@
+import numpy as np
+
+
+class Tile():
+    """tiling of feature space"""
+
+    def __init__(self, min, max, stepsize):
+        self.min = min
+        self.max = max
+        self.range = abs(min - max)
+        self.stepsize = stepsize
+        self.tiling = np.arange(min, max, stepsize)
+
 
 # define params for tiling of feature space
+feature_space = Tile(min=-90, max=90, stepsize=0.05)
 
 # define params for stimuli values
 
@@ -8,12 +22,12 @@
 # define params for response of neural populations
 
 # each neural population as instance of class
-    # boundaries
-    # bandwidth
-    # sampling frequency
-    # max firing rate
-    # preferences
-    # genTunings()
+# boundaries
+# bandwidth
+# sampling frequency
+# max firing rate
+# preferences
+# genTunings()
 
 # adjust boundaries of each population based on sampling frequencies
 
@@ -22,4 +36,3 @@
 # generate response for allPops from stim_vals
 
 # decode allPops responses (WTA, PV, ML)
-
