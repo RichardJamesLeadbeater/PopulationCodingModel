021de77b367535867435b659bb1398c900023c82
RichardJamesLeadbeater <@1Wanderers>
2021-04-26 09:32:11 +0100

    everything completed up until PopResponse

--- Population_Coding_Model.py
+++ Population_Coding_Model.py
@@ -23,6 +23,16 @@ def find_nearest(array, values):  # nearest value and index in array for each va
     return nearest_values, nearest_idx
 
 
+def sort_by_standard(standard, *args):
+    # sorts arrays with inds matching ascending order of standard
+    inds = standard.argsort()  # gets indices of sorted standard
+    sorted_arrays = [standard[inds[::1]]]  # create list with first element as sorted standard
+    for arg in args:
+        sorted_arrays.append(arg[inds[::1]])  # append arrays sorted to inds of standard
+    # sorted in ascending numerical order [::1]
+    return sorted_arrays
+
+
 def rolling_window(vector, window):
     # create rolling window across vector
     m = len(vector) - len(window)
@@ -50,29 +60,29 @@ class Params:
 class Tiling:
     """tiling of feature space"""
 
-    def __init__(self, min_val, max_val, stepsize):
-        self.min_val = min_val
-        self.max_val = max_val
-        self.range = abs(min_val - max_val)
+    def __init__(self, min_tile, max_tile, stepsize):
+        self.min_val = min_tile
+        self.max_val = max_tile
+        self.range = abs(min_tile - max_tile)
         self.stepsize = stepsize
-        self.tiling = np.arange(min_val, max_val, stepsize)
+        self.tiling = np.arange(min_tile, max_tile, stepsize)
 
 
 class Stimuli:
     """values of MyStimuli across feature space"""
 
-    def __init__(self, n_stim, min_val, max_val, tiling, distribution='rand'):
+    def __init__(self, n_stim, min_stim, max_stim, tiling, distribution='rand'):
         self.n_stim = n_stim
-        self.min_val = min_val
-        self.max_val = max_val
+        self.min_val = min_stim
+        self.max_val = max_stim
         self.tiling = tiling
 
         self.distribution = distribution
         if distribution == 'unif':  # achieves near-equal spacing of stim_vals
-            stim_freq = abs(min_val - max_val) / n_stim
-            self.stim_vals = np.linspace(min_val, max_val - stim_freq, n_stim)  # no repeats e.g. -90 and 90
+            stim_freq = abs(min_stim - max_stim) / n_stim
+            self.stim_vals = np.linspace(min_stim, max_stim - stim_freq, n_stim)  # no repeats e.g. -90 and 90
         elif distribution == 'rand':  # random spacing of stim_vals
-            self.stim_vals = np.random.uniform(min_val, max_val, n_stim)
+            self.stim_vals = np.random.uniform(min_stim, max_stim, n_stim)
 
         # nearest val & idx in array for each given value
         self.stim_vals, self.stim_indices = find_nearest(tiling, self.stim_vals)
@@ -81,7 +91,7 @@ class Stimuli:
 class NeuralPopulation:
     """params and sampling for neural population with independent properties"""
 
-    def __init__(self, name, boundaries, sampling_freq, sigma=10, rmax=60, spont=0.05):
+    def __init__(self, name, boundaries, sampling_freq, sigma=10, r_max=60, spont=0.05):
         self.tunings = None
         self.prefs = None
         self.prefs_idx = None
@@ -89,7 +99,7 @@ class NeuralPopulation:
         self.boundaries = boundaries  # [min, max]
         self.sampling_freq = sampling_freq  # degrees per neuron pref
         self.sigma = sigma  # sigma of gaussian tuning
-        self.rmax = rmax
+        self.r_max = r_max
         self.spont = spont
 
     def generate_prefs(self, tiling):
@@ -129,12 +139,16 @@ class NeuralPopulation:
         repeated_tiling = tiling * np.ones([len(self.prefs), 1])
         tunings = generate_gaussian(repeated_tiling, self.prefs, self.sigma)
         tunings = tunings / tunings.max()  # normalise to 1
-        tunings = tunings * self.rmax + (self.spont * self.rmax)  # normalise to rmax then + spont firing
+        tunings = tunings * self.r_max + (self.spont * self.r_max)  # normalise to rmax then + spont firing
         self.tunings = tunings
         return self.tunings
 
 
-# todo conversion of prefs to nearest val in tiling - can be done once prefs vector is joined together
+class PopulationTuning:
+    def __init__(self, prefs, prefs_idx, tunings):
+        self.prefs = prefs
+        self.prefs_idx = prefs_idx
+        self.tunings = tunings
 
 
 def get_boundaries(mid_values, bound_range):
@@ -183,68 +197,54 @@ def adjust_boundaries(boundaries, adjuster_values):
     return adjusted_bounds
 
 
+Cardinal = {'sampling_freq': 1, 'sigma': 10, 'r_max': 60, 'spont': 0.05}
+Oblique = {'sampling_freq': 1, 'sigma': 10, 'r_max': 60, 'spont': 0.05}
+
+
 ori_populations = {'vertical': NeuralPopulation(name='vertical', boundaries=get_boundaries([-90, 90], 45),
-                                                sampling_freq=1, sigma=10,
-                                                spont=0.05, rmax=60),
+                                                sampling_freq=Cardinal['sampling_freq'], sigma=Cardinal['sigma'],
+                                                spont=Cardinal['spont'], r_max=Cardinal['r_max']),
 
                    'right_oblique': NeuralPopulation(name='right_oblique', boundaries=get_boundaries([-45, 135], 45),
-                                                     sampling_freq=1, sigma=10,
-                                                     spont=0.05, rmax=60),
+                                                     sampling_freq=Oblique['sampling_freq'], sigma=Oblique['sigma'],
+                                                     spont=Oblique['spont'], r_max=Oblique['r_max']),
 
                    'horizontal': NeuralPopulation(name='horizontal', boundaries=get_boundaries([-180, 0, 180], 45),
-                                                  sampling_freq=1, sigma=10,
-                                                  spont=0.05, rmax=60),
+                                                  sampling_freq=Cardinal['sampling_freq'], sigma=Cardinal['sigma'],
+                                                  spont=Cardinal['spont'], r_max=Cardinal['r_max']),
 
                    'left_oblique': NeuralPopulation(name='left_oblique', boundaries=get_boundaries([-135, 45], 45),
-                                                    sampling_freq=1, sigma=10,
-                                                    spont=0.05, rmax=60)
+                                                    sampling_freq=Oblique['sampling_freq'], sigma=Oblique['sigma'],
+                                                    spont=Oblique['spont'], r_max=Oblique['r_max']),
                    }
 
-sampling_freqs = [ori_populations[x].sampling_freq for x in ori_populations]
-
-freq_avgs = adjust_adjacents(value_list=[ori_populations[x].sampling_freq for x in ori_populations])
-
+# adjust boundaries using avg sampling rates of neighbouring populations
 bounds_adjusted = adjust_boundaries(boundaries=[ori_populations[x].boundaries for x in ori_populations],
-                                    adjuster_values=freq_avgs)
-
+                                    adjuster_values=adjust_adjacents(
+                                        [ori_populations[x].sampling_freq for x in ori_populations]))
+# assign adjusted boundaries to associated ori-tuned population
 for idx, ori in enumerate(ori_populations):
     ori_populations[ori].boundaries = bounds_adjusted[idx]
 
-# define params for tiling of feature space
-FeatureSpace = Tiling(min_val=-180, max_val=180, stepsize=0.05)
-
-# define params for MyStimuli values within feature space
-MyStimuli = Stimuli(n_stim=100, min_val=-90, max_val=90 - FeatureSpace.stepsize, tiling=FeatureSpace.tiling,
+# define params for tiling of extended feature space (e.g. ori is from -90 to 90, so have -180 to 180)
+FeatureSpace = Tiling(min_tile=-180, max_tile=180, stepsize=0.05)
+# define params for MyStimuli values within normal feature space (e.g. ori stim = [-90:90]
+MyStimuli = Stimuli(n_stim=100, min_stim=-90, max_stim=90-FeatureSpace.stepsize, tiling=FeatureSpace.tiling,
                     distribution='rand')
 
-PopulationResponse = Params()
-PopulationResponse.prefs = np.asarray([])
-PopulationResponse.prefs_idx = np.asarray([])
-PopulationResponse.tunings = []
-# concatenate prefs, prefs_idx, tunings in the same order (can only sort if we can sort tunings in the same pattern)
+PopTuning = PopulationTuning(prefs=np.asarray([]), prefs_idx=np.asarray([]), tunings=[])
 for idx, ori in enumerate(ori_populations):
     ori_populations[ori].generate_prefs(tiling=FeatureSpace.tiling)  # prefs for each ori
     ori_populations[ori].generate_tunings(tiling=FeatureSpace.tiling)  # tunings for each ori
+    # concatenate prefs, prefs_idx, tunings
+    PopTuning.prefs = np.hstack([PopTuning.prefs, ori_populations[ori].prefs])
+    PopTuning.prefs_idx = np.hstack([PopTuning.prefs_idx, ori_populations[ori].prefs_idx])
+    PopTuning.tunings.append(ori_populations[ori].tunings)  # rows=prefs, cols=tiling, vals=tuned_response
+PopTuning.tunings = np.vstack([i for i in PopTuning.tunings])
+# sort prefs_idx and tunings by indices of prefs in ascending order
+(PopTuning.prefs, PopTuning.prefs_idx, PopTuning.tunings) = sort_by_standard(PopTuning.prefs, PopTuning.prefs_idx,
+                                                                             PopTuning.tunings)
 
-    PopulationResponse.prefs = np.hstack([PopulationResponse.prefs, ori_populations[ori].prefs])  # all prefs
-    PopulationResponse.prefs_idx = np.hstack([PopulationResponse.prefs_idx, ori_populations[ori].prefs_idx])  # all idx
-    PopulationResponse.tunings.append(ori_populations[ori].tunings)  # rows=prefs, cols=tiling, vals=tuned_response
-
-PopulationResponse.tunings = np.vstack([i for i in PopulationResponse.tunings])
-
-
-def sort_by_standard(standard, *args):
-    inds = standard.argsort()  # gets indices of sorted standard
-    sorted_arrays = [standard[inds[::1]]]  # create list with first element as sorted standard
-    for arg in args:
-        sorted_arrays.append(arg[inds[::1]])  # append arrays sorted to inds of standard
-    # sorted in ascending numerical order [::1]
-    return sorted_arrays
-
-sorted_arrays = sort_by_standard(PopulationResponse.prefs, PopulationResponse.prefs_idx, PopulationResponse.tunings)
-PopulationResponse.prefs = sorted_arrays[0]
-PopulationResponse.prefs_idx = sorted_arrays[1]
-PopulationResponse.tunings = sorted_arrays[2]
 print('debug')
 
 # generate response for allPops from stim_vals
